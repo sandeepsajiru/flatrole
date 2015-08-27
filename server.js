4714700dcf766622b578031239c2f15e5e5e302f
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2015-08-27 02:40:43 -0700

    Adding Stylus, BodyParser and Logger

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var port = 3000;
 
+
 var app = express();
+function compile(str, path)
+{
+    return stylus(str).set('filename', path);
+}
 
 app.set('views', __dirname+'/server/views');
 app.set('view engine', 'jade');
+
+app.use(stylus.middleware(
+    {
+        src:__dirname +'/public', compile:compile
+    }
+));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res){
    res.render('index');
